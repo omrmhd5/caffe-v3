9d0a6e6f28d4e335bab3bae0796208635907bbeb
omrmhd5 <omrmhd54@gmail.com>
2025-07-30 02:07:39 +0300

    Rent Comments Fixed Size, Added Salary Comments To the Print View, Auto Set Start and End Dates in Custodies/Invoices

--- src/salaryNote/service.js
+++ src/salaryNote/service.js
@@ -1,20 +1,33 @@
 const Note = require("../../models/salaryNote");
+const { toMonthStartDate } = require("../common/date");
 
 exports.addBulk = async (notes) => {
   if (notes.length > 0) {
+    // Normalize the date to the first day of the month
+    const normalizedDate = toMonthStartDate(notes[0].date);
+
     await Note.deleteMany({
-      date: notes[0].date,
+      date: normalizedDate,
       branchID: notes[0].branchID,
     });
 
-    await Note.insertMany(notes);
+    // Update all notes to use the normalized date
+    const notesWithNormalizedDate = notes.map((note) => ({
+      ...note,
+      date: normalizedDate,
+    }));
+
+    await Note.insertMany(notesWithNormalizedDate);
   }
 };
 
 exports.getNotes = async (branchID, date, user) => {
+  // Normalize the date to the first day of the month
+  const normalizedDate = toMonthStartDate(date);
+
   let notes = await Note.find({
     branchID,
-    date,
+    date: normalizedDate,
   }).lean();
 
   notes = notes.map((note) => {
